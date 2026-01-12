5747e2185dd8a4cda40196061209dc89915bc86e
JINX <92461540+0Linxa0@users.noreply.github.com>
2026-01-12 08:11:30 +0000

    zrobione zaliczenie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,23 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Podaj tekst: ");
+    String txt = sc.nextLine();
+    String reverse= new StringBuilder(txt).reverse().toString();
+    System.out.println(reverse);
+    if (reverse.equalsIgnoreCase(txt)){
+      System.out.println("Jest palindromem");
+    }
+    else {
+      System.out.println("Nie jest palindromem");
+    }
+    
+    
+
+    
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+ 
 }
